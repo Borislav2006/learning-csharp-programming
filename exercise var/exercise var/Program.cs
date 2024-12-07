c01fb0c60933074780475b6f742b7ecd8881e89e
Borislav Ivanov <borislav.project@gmail.com>
2024-12-07 12:40:59 +0200

    var element

---
+++ exercise var/exercise var/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_var
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Borislav";
+            var number = "0123456789";
+            var age = 18;
+
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
+
+            Console.ReadLine();
+        }
+    }
+}
