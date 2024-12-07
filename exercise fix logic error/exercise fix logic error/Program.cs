0fe3eeb0ce65e30162e46ac0231a1e3b0687f384
Borislav Ivanov <borislav.project@gmail.com>
2024-12-07 12:53:19 +0200

    find the logic errror

---
+++ exercise fix logic error/exercise fix logic error/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_fix_logic_error
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 35;
+            if(age >18)
+            {
+                Console.WriteLine("Over 18");
+            }
+            else if (age > 30)
+            {
+                Console.WriteLine("Over 30");
+            }
+            else if (age > 50)
+            {
+                Console.WriteLine("Over 50");
+            }
+            Console.ReadLine();
+        }
+    }
+}
