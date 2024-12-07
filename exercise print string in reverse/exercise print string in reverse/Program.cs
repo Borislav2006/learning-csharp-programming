e36d5a23a7a9ef5f8aa1a7d9981cd7462239dab1
Borislav Ivanov <borislav.project@gmail.com>
2024-12-07 12:48:54 +0200

    string in reverse

---
+++ exercise print string in reverse/exercise print string in reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_print_string_in_reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type something: ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]); 
+            }
+            Console.Write("-");
+            for (int i = text.Length - 1; i >=0; i--)
+            {
+                Console.Write(text[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
