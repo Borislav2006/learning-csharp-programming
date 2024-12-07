636d51f30e84619ecf0638481ab258e5b9930a6a
Borislav Ivanov <borislav.project@gmail.com>
2024-12-07 12:44:27 +0200

    odd or even number

---
+++ exercice odd or even/exercice odd or even/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_odd_or_even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;
+            int num2 = 10;
+            int sum =num1 % num2 ;
+            Console.WriteLine(sum);
+
+            int number1 = 13;
+            int number2 = 2;
+            int remeinder = number1 % number2 ;
+            Console.WriteLine(remeinder);
+
+            Console.ReadLine();
+        }
+    }
+}
